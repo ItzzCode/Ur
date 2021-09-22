a265004c1d290d982c9b7973dfe7623ce2e93dbf
ItzzCode <51547168+ItzzCode@users.noreply.github.com>
2021-09-22 18:38:47 -0400

    Update main.py
    more stuff

--- main.py
+++ main.py
@@ -38,32 +38,50 @@ plr=[
         [
             "Jesse", #name
             6, #defense treshold
-            0,0, #x, y
-            True, #has ball
-            False #down
-        ],
-        [
-            "Col", #name
-            4, #defense treshold
-            0,4, #x, y
+            15,0, #x, y
             False, #has ball
             False #down
         ],
         [
-            "Beth", #name
+            "Mel", #name
+            3, #defense treshold
+            15,4, #x, y
+            False, #has ball
+            False #down
+        ],
+        [
+            "Sam", #name
             7, #defense treshold
-            0,8, #x, y
+            15,8, #x, y
             False, #has ball
             False #down
         ],
         [
-            "Jen", #name
+            "Mike", #name
             6, #defense treshold
-            0,12, #x, y
+            15,12, #x, y
             False, #has ball
             False #down
-        ] 
         ]
     ]
+]]]
 
+game=[
+    ["X","X","X","-","-","-","x","x","x","-","-","-","x","x","x","X","X","X",],
+    ["X","X","X","-","-","-","x","x","x","-","-","-","x","x","x","X","X","X",],
+    ["X","X","X","-","-","-","x","x","x","-","-","-","x","x","x","X","X","X",],
+    ["X","X","X","-","-","-","x","x","x","-","-","-","x","x","x","X","X","X",],
+    ["X","X","X","-","-","-","x","x","x","-","-","-","x","x","x","X","X","X",],
+    ["X","X","X","-","-","-","x","x","x","-","-","-","x","x","x","X","X","X",],
+    ["X","X","X","-","-","-","x","x","x","-","-","-","x","x","x","X","X","X",],
+    ["X","X","X","-","-","-","x","x","x","-","-","-","x","x","x","X","X","X",],
+    ["X","X","X","-","-","-","x","x","x","-","-","-","x","x","x","X","X","X",],
+    ["X","X","X","-","-","-","x","x","x","-","-","-","x","x","x","X","X","X",],
+    ["X","X","X","-","-","-","x","x","x","-","-","-","x","x","x","X","X","X",],
+    ["X","X","X","-","-","-","x","x","x","-","-","-","x","x","x","X","X","X",],
 ]
+
+for i, line in enumerate(game):
+    for char in line:
+        print(char,end="")
+    print()
