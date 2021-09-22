69a4c19a1960135e7d2fa2e2ac00f24792e45f54
ItzzCode <51547168+ItzzCode@users.noreply.github.com>
2021-09-22 21:53:03 +0000

    hkesfndjk

---
+++ main.py
@@ -0,0 +1,69 @@
+#data
+plr=[
+    #players team
+    [
+        [
+            "Jack", #name
+            6, #defense treshold
+            0,0, #x, y
+            True, #has ball
+            False #down
+        ],
+        [
+            "Col", #name
+            4, #defense treshold
+            0,4, #x, y
+            False, #has ball
+            False #down
+        ],
+        [
+            "Beth", #name
+            7, #defense treshold
+            0,8, #x, y
+            False, #has ball
+            False #down
+        ],
+        [
+            "Jen", #name
+            6, #defense treshold
+            0,12, #x, y
+            False, #has ball
+            False #down
+        ]
+    ],
+    #enemies team
+    [
+        [
+           [
+        [
+            "Jesse", #name
+            6, #defense treshold
+            0,0, #x, y
+            True, #has ball
+            False #down
+        ],
+        [
+            "Col", #name
+            4, #defense treshold
+            0,4, #x, y
+            False, #has ball
+            False #down
+        ],
+        [
+            "Beth", #name
+            7, #defense treshold
+            0,8, #x, y
+            False, #has ball
+            False #down
+        ],
+        [
+            "Jen", #name
+            6, #defense treshold
+            0,12, #x, y
+            False, #has ball
+            False #down
+        ] 
+        ]
+    ]
+
+]
